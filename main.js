d736033d3409f54495a3f6fd950e3180b7d6c288
noelensith <mbnoelen@gmail.com>
2022-05-07 22:38:06 -0700

    Completed Assignment

--- main.js
+++ main.js
@@ -3,6 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+//this const hearts selects the like glyph for all the glyphs on the page
+const hearts = document.querySelectorAll('.like-glyph');
+//fucnction likeHeart takes in an element being the glyph that we are targeting
+// callsback the function mimic server, after the function runs we use .then
+//which is takes in an anonymous function that checks to see whether the heartAction is empty
+//if so since we clicked it we change the innerText to become full, and also add the classname 'activated-heart'
+//if the full heart is unliked it returns an empty glyph
+function likeHeart(e) {
+  const heartAction = e.target;
+  mimicServerCall("")
+    .then(() => {
+      if (heartAction.innerText === EMPTY_HEART) {
+        heartAction.innerText = FULL_HEART;
+        heartAction.className = 'activated-heart';
+      } else {
+        heartAction.innerText = EMPTY_HEART;
+        heartAction.className = '';
+      }
+    })
+//catch is meant to display error message if error occurs
+    .catch((error) => {
+      const modal = document.getElementById('modal');
+      modal.className = '';
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = 'hidden', 3000);
+    });
+}
+//eventlistener listens to check for each glyph of hearts is clicked, if so run likeHeart
+for (const glyph of hearts) {
+  glyph.addEventListener("click", likeHeart);
+}
 
 
 
